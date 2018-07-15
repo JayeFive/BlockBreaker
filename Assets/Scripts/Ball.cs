49386c4935a0fe13f728abeeda41eeb60ef5d43e
John Olson <john.olson3@gmail.com>
2018-07-15 11:07:39 -0700

    initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    // config params
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float startingXVector = 2.0f;
+    [SerializeField] float startingYVector = 10.0f;
+
+
+    // state
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(startingXVector, startingYVector);
+            hasStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    // Config params
+
+    [SerializeField] float screenWidthInUnits = 16.0f;
+    [SerializeField] float clamp_left = 1f;
+    [SerializeField] float clamp_right = 15f;
+
+    // Update is called once per frame
+    void Update () {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, clamp_left, clamp_right);
+        transform.position = paddlePos;
+	}
+}
