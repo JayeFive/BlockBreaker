7a17bc6778ba4243014d49ceb015fd2a34293e10
John Olson <john.olson3@gmail.com>
2018-07-15 23:52:15 -0700

    gameplay tweaks

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -9,16 +9,17 @@ public class Ball : MonoBehaviour {
     [SerializeField] float startingXVector = 2.0f;
     [SerializeField] float startingYVector = 10.0f;
     [SerializeField] int sideForce = 500;
+    [SerializeField] float maxSpeed = 17f;
     [SerializeField] Rigidbody2D rb;
 
 
     // state
-    Vector2 paddleToBallVector;
     bool hasStarted = false;
 
     // paddle position data
     Paddle paddle1;
     float paddleXPos;
+    Vector2 paddleToBallVector;
 
 	// Use this for initialization
 	void Start ()
@@ -26,17 +27,31 @@ public class Ball : MonoBehaviour {
         paddle1 = GameObject.FindObjectOfType<Paddle>();
         paddleToBallVector = transform.position - paddle1.transform.position;
         rb = GetComponent<Rigidbody2D>();
+
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+
         if(!hasStarted)
         {
             LockBallToPaddle();
             LaunchOnMouseClick();
         }
         Vector2 ballPos = new Vector2(transform.position.x, transform.position.y);
+
+        //if (rb.velocity.magnitude > maxSpeed)
+        //{
+        Debug.Log(rb.velocity.magnitude);
+        //    rb.velocity = rb.velocity.normalized * maxSpeed;
+        //}
+    }
+
+    void FixedUpdate ()
+    {
     }
 
     private void LaunchOnMouseClick()
@@ -56,10 +71,10 @@ public class Ball : MonoBehaviour {
 
     private void OnCollisionEnter2D (Collision2D collision)
     {
-        if (collision.gameObject.tag.Equals("Paddle") == true)
+        if (collision.gameObject.tag.Equals("Paddle"))
         {
             paddleXPos = paddle1.transform.position.x;
-            Vector2 distanceFromPaddleCenter = new Vector2(transform.position.x - paddleXPos, 0);
+            Vector2 distanceFromPaddleCenter = new Vector2(transform.position.x - paddleXPos, 100 / sideForce);
             rb.AddForce(distanceFromPaddleCenter * sideForce);
         }
     }
--- Assets/Scripts/GamePlay.cs
+++ Assets/Scripts/GamePlay.cs
@@ -6,10 +6,18 @@ using UnityEngine.SceneManagement;
 public class GamePlay : MonoBehaviour {
 
     public int blockCount;
+    
 
     void Start ()
     {
         Cursor.visible = false;
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == "StartMenu" || currentScene.name == "GameOver")
+        {
+            Cursor.visible = true;
+        }
+
         blockCount = GameObject.FindGameObjectsWithTag("Block").Length;
     }
 
