2851ae475909ef9efa1b275cad8c60d6bf713c6d
John Olson <john.olson3@gmail.com>
2018-07-17 15:34:28 -0700

    Add gameplay sounds

---
+++ Assets/Scripts/BlockCollisionSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockCollisionSound : MonoBehaviour {
+
+    [SerializeField] AudioClip pop;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = pop;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Block"))
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
---
+++ Assets/Scripts/PaddleCollisionSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleCollisionSound : MonoBehaviour {
+
+    [SerializeField] AudioClip bounce;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = bounce;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Ball"))
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
