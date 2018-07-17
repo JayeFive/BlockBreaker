00950ee756b93e082455e4c9901a0659f3a2ae21
John Olson <john.olson3@gmail.com>
2018-07-17 10:22:29 -0700

    Tape stop functionality on lose life

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -11,12 +11,6 @@ public class LoseCollider : MonoBehaviour {
     [SerializeField] Paddle paddle;
     [SerializeField] TapeStop tapeStop;
 
-    void Start ()
-    {
-        tapeStop.bgMusic = FindObjectOfType<AudioSource>();
-        Debug.Log(tapeStop.bgMusic.volume);
-    }
-
     private void OnTriggerEnter2D (Collider2D collision)
     {
         tapeStop.InitiateTapeStop();
---
+++ Assets/Scripts/TapeStop.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapeStop : MonoBehaviour {
+
+    public float startingPitch = 1;
+    public float timeToChange = 1;
+    bool isTapeStopping = false;
+    bool isTapeStarting = false;
+    AudioSource bgMusic;
+
+	// Use this for initialization
+	void Start () {
+        bgMusic = FindObjectOfType<MusicPlayer>().GetComponent<AudioSource>();
+        bgMusic.pitch = startingPitch;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isTapeStopping)
+        {
+            StopTape();
+        }
+
+        if (isTapeStarting)
+        {
+            StartTape();
+        }
+    }
+
+    public void InitiateTapeStop ()
+    {
+        isTapeStopping = true;
+    }
+
+    void StopTape ()
+    {
+        if (bgMusic.pitch > 0)
+        {
+            bgMusic.pitch -= Time.deltaTime * startingPitch / timeToChange;
+        }
+        else
+        {
+            isTapeStopping = false;
+            isTapeStarting = true;
+        }
+    }
+
+    void StartTape()
+    {
+        if (bgMusic.pitch < startingPitch)
+        {
+            bgMusic.pitch += Time.deltaTime * startingPitch / timeToChange;
+        }
+        else
+        {
+            isTapeStarting = false;
+            bgMusic.pitch = startingPitch;
+        }
+    }
+}
